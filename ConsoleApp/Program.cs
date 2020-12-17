b8c98bd93c6842c10da1226d7bea8dccc5c7ecd0
omercanxx <omer60can@hotmail.com>
2020-12-17 19:05:58 +0300

    gRPC
    error with generate different number

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using gRPC;
+using Grpc.Net.Client;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+            
+
+            var response = await client.SayHelloAsync(
+                new HelloRequest
+                {
+                    Name = "Ömer Can",
+                    Surname = "Sucu"
+                });
+            Console.WriteLine("From Server: " + response.Message);
+            Console.WriteLine("1 - Get Time");
+            Console.WriteLine("2 - Prediction Game");
+            int caseSwitch = Convert.ToInt32(Console.ReadLine());
+            switch (caseSwitch)
+            {
+                case 1:
+                    var responseSwitch = await client.GetTimeAsync(
+                        new TimeRequest { });
+                    Console.WriteLine("Time:" + responseSwitch.Year+ "//" + responseSwitch.Month + "//" + responseSwitch.Day);
+                    break;
+                case 2:
+                    while (true)
+                    {
+                        Console.WriteLine("Enter a number");
+                        string number = Console.ReadLine();
+                        var responseSwitch2 = await client.PlayGameAsync(
+                            new GameRequest
+                            {
+                                Message = number
+                            }
+                        );
+                        string msg = responseSwitch2.Message;
+                        Console.WriteLine(msg);
+                        if(msg == "WIN")
+                        {
+                            break;
+                        }
+                    }
+                    break;
+                    
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPC/Services/GreeterService.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace gRPC
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name + " " + request.Surname
+            });
+        }
+        public override Task<TimeReply> GetTime(TimeRequest request, ServerCallContext context)
+        {
+            string year = (DateTime.UtcNow.Year).ToString();
+            string day = (DateTime.UtcNow.Day).ToString();
+            string month = (DateTime.UtcNow.Month).ToString();
+            return Task.FromResult(new TimeReply
+            {
+                Day = "Day:" + day,
+                Month = "Month:" + month,
+                Year = "Year: " + year,
+            });
+        }
+        public override Task<GameReply> PlayGame(GameRequest request, ServerCallContext context)
+        {
+            Random r = new Random();
+            int rInt = r.Next(0, 100);
+
+            Console.WriteLine(rInt);
+            if (Convert.ToInt32(request.Message) > rInt)
+            {
+                return Task.FromResult(new GameReply
+                {
+                    Message = "GTH"
+                });
+
+            }
+            else if (Convert.ToInt32(request.Message) < rInt)
+            {
+                return Task.FromResult(new GameReply
+                {
+                    Message = "LTH"
+                });
+            }
+            else
+            {
+                return Task.FromResult(new GameReply
+                {
+                    Message = "WIN"
+                });
+            }
+
+        }
+    }
+}
